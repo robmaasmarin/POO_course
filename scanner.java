7ed120a1150a434f4dbd09025509047c64e282a1
robmaasmarin <roberabp@gmail.com>
2024-02-21 07:09:26 +0100

    first commit

---
+++ scanner.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        System.out.println("Welcome to this Java application");
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int firstNumber = myScanner.nextInt();
+        System.out.println("Enter another number: ");
+
+        int secondNumber = myScanner.nextInt();
+
+        /*while (secondNumber <= firstNumber) {
+            int resta = firstNumber - secondNumber;
+            System.out.println("Resultado =  " + resta);
+
+               }*/
+        do {
+            System.out.println("Sorry, invalid numbers. Please enter a new number: ");
+            firstNumber = myScanner.nextInt();
+            System.out.println("Enter another number: ");
+            secondNumber = myScanner.nextInt();
+            continue;
+            
+        } while (secondNumber> firstNumber);
+        
+        int resta = firstNumber - secondNumber;
+        System.out.println("Resultado =  " + resta);
+        myScanner.close();
+    }
+}
